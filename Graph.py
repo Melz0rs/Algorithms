1613dd7b973faaea57095de8806a056f24c5a9e1
Carmel Shupak <carmelshupak@gmail.com>
2019-05-19 14:51:00 +0300

    graph ds wip

---
+++ Graph.py
@@ -0,0 +1,18 @@
+class Graph:
+
+    def __init__(self):
+        # init edges & vertices
+
+
+    def add_node(self):
+
+
+    def add_edge(self):
+
+
+    def union(self):
+
+
+    def find_parent(self):
+
+    
