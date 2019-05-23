903582be8cc12cf0a2b5f33853d14a0157dc4cd3
Carmel Shupak <carmelshupak@gmail.com>
2019-05-23 15:17:43 +0300

    bubble sort

---
+++ common/sorts/bubble_sort.py
@@ -0,0 +1,18 @@
+def bubble_sort(arr):
+    sorted_arr = arr.copy()
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if sorted_arr[j] > sorted_arr[j + 1]:
+                sorted_arr[j], sorted_arr[j + 1] = sorted_arr[j + 1], sorted_arr[j]
+
+    return sorted_arr
+
+
+arr = [4, 5, 56, 12, 87, 2, 0, 99, 111, 43]
+
+sorted_arr = bubble_sort(arr)
+
+print(f"sorted_arr: {sorted_arr}")
+
--- graph/kruskal.py
+++ graph/kruskal.py
@@ -1,4 +1,5 @@
 from common.sorts.merge_sort import merge_sort
+from graph.undirected.is_cyclic import is_cyclic
 
 def kruskal(nodes, edges, included_edges=None):
     if included_edges is None:
